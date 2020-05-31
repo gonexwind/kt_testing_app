7d7c854a04d68b1aeb06104461e601e19a6118b7
gonexwind <fikky.ardi15@gmail.com>
2020-06-01 02:50:19 +0700

    debuging

---
+++ app/src/main/java/com/fikkyardianto/mytestingapp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.fikkyardianto.mytestingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
+    private lateinit var imgPreview: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue = findViewById(R.id.btn_set_value)
+        btnSetValue.setOnClickListener(this)
+
+        imgPreview = findViewById(R.id.img_preview)
+//        imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big))
+        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
+
+        names.add("Fikky")
+        names.add("Ardi")
+        names.add("Anto")
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_set_value) {
+            val name = StringBuilder()
+            for (i in 0..2) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
+    }
+}
